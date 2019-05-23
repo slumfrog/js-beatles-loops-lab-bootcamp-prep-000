a22b3186f81007bc22f5fcfaad70d0b3f1cb7cb8
Anil Kishan <kishan.anil@gmail.com>
2019-05-23 00:49:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,4 +18,13 @@ function johnLennonFacts(arr) {
 }
 
 
-
+function iLoveTheBeatles(x) {
+  var array = [];
+  do {
+    x = array.push("I love the Beatles!");
+    x++;
+  } while (x < 15); {
+    
+  }
+  return array;
+}
