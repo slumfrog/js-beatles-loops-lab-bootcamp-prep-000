ae8d1edeaab124bab25f58272b196d7c7157c70d
Anil Kishan <kishan.anil@gmail.com>
2019-05-23 00:42:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+    var empty = [];
+    for (var i = 0; i < musicians.length; i++) {
+        empty.push(musicians[i] + " plays " + instruments[i]); 
+    }
+    return empty
+}
+
+
+function johnLennonFacts(arr) {
+    var result= [];        // create an empty array called result
+    var i = 0;              // set our counter to 0
+    while(i < arr.length) { // if our counter is less than the number of quotes
+    result.push(arr[i] + '!!!') // store in array called results, and add '!!!'
+    i++;                        // loops to beginning
+  }
+  return result;            // returns result
+}
+
+
+function iLoveTheBeatles(num){
+  var love = []
+  do { 
+    love.push("I love the Beatles!")
+  }
+    while (var i = 0; i < num; i++)
+  return love;
+}
+
+
